94b5170017c8f469a6d0fc2b5c79a7f4aafc944d
Vini <vsgh-gouveia@hotmail.com>
2023-05-04 15:13:26 -0300

    Melhoria CSS contato; mudança de title

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -10,34 +10,34 @@ export default function Contato() {
         </p>
       </div>
 
-        <p className="font-sourcesanspro text-xl font-bold">
+        <p className="font-sourcesanspro text-xl font-bold text-secondary">
           Não fique com vergonha, entre em contato!
         </p>
 
 
-      <form action="https://formsubmit.co/2eedad7ce33b62d9817e39c65d1a6cd9" method="POST" className="w-8/12 flex flex-col gap-1 py-10 font-sourcesanspro">
+      <form action="https://formsubmit.co/2eedad7ce33b62d9817e39c65d1a6cd9" method="POST" className="w-full flex flex-col gap-1 py-10 font-sourcesanspro">
 
 
         <input type="hidden" name="_template" value="box" />
 
         <div className="flex gap-1">
-          <div className="flex flex-col">
+          <div className="flex flex-col w-1/2">
             <label htmlFor="name" className="px-2">Nome</label>
             <input type="text" name="name" placeholder="John Doe"
-              className="bg-neutral-800 py-1 px-3 text-lg placeholder:text-neutral-600 rounded-ss-xl rounded-es rounded-se rounded-ee outline-none focus:border focus:border-neutral-600" required />
+              className="bg-neutral-800 py-1 px-3 text-lg placeholder:text-neutral-600 rounded rounded-ss-xl outline-none focus:border focus:border-neutral-600 " required />
 
           </div>
 
-          <div className="flex flex-col">
+          <div className="flex flex-col w-1/2">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" placeholder="email@exemplo.com"
-            className="bg-neutral-800 py-1 px-3 text-lg placeholder:text-neutral-600 rounded-ss rounded-es rounded-se-xl rounded-ee outline-none focus:border focus:border-neutral-600" required />
+            className="bg-neutral-800 py-1 px-3 text-lg placeholder:text-neutral-600 rounded rounded-se-xl outline-none focus:border focus:border-neutral-600" required />
           </div>
 
         </div>
         <textarea name="message" cols="40" rows="10" placeholder="Sua mensagem"
-          className="w-full bg-neutral-800 py-1 px-3 text-lg resize-none placeholder:text-neutral-600 rounded-ss rounded-es rounded-se-xl rounded-ee-xl outline-none focus:border focus:border-neutral-600" required></textarea>
-        <button type="submit" className="bg-primary text-neutral-800 w-fit px-7 py-2 rounded self-start font-semibold">Enviar Email</button>
+          className="w-full bg-neutral-800 py-1 px-3 text-lg resize-none placeholder:text-neutral-600 rounded outline-none focus:border focus:border-neutral-600" required></textarea>
+        <button type="submit" className="bg-primary text-neutral-800 w-full px-7 py-2 rounded rounded-es-xl rounded-ee-xl self-start font-semibold hover:bg-tertiary transition-all">Enviar Email</button>
       </form>
     </div>
   )
