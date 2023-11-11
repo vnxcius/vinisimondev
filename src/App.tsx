68f3d5db28d1e2b8aa4b2f06f557919281e11d46
Vini <vsgh-gouveia@hotmail.com>
2023-11-11 01:33:50 -0300

    fix - Removed projects.tsx imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
-import Projects from './pages/Projects'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/projetos" element={<Projects />} />
 
       <Route path='/404' element={<h1>Not Found</h1>} />
       <Route path='*' element={<Navigate to='/404' />} />
