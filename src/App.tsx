ac879e94c8a157200bad38b0e2231e9400cf3bbb
Vini <vsgh-gouveia@hotmail.com>
2023-11-11 01:29:26 -0300

    Remove unnecessary fragment App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
+import Projects from './pages/Projects'
 
 function App() {
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projetos" element={<Projects />} />
 
-        <Route path='/404' element={<h1>Not Found</h1>} />
-        <Route path='*' element={<Navigate to='/404' />} />
-         
-      </Routes>
-    </>
+      <Route path='/404' element={<h1>Not Found</h1>} />
+      <Route path='*' element={<Navigate to='/404' />} />
+
+    </Routes>
   )
 }
 
