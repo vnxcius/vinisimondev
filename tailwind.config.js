00b535550128f17738970a7b64669bd85c346bf3
Vini <vsgh-gouveia@hotmail.com>
2023-11-11 01:30:39 -0300

    tailwind - added dark color and removed colors and fontfamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        accent: "#15aaff", // Azul
-        primary: "#373636", // Cinza
-        secondary: "#f8f7f3", // Branco
+        dark: "#101010", // Preto
       },
       fontFamily: {
-        opensans: "'Open Sans', sans-serif",
-        archivoblack: "'Archivo Black', sans-serif",
         sourcesans: "'Source Sans 3', sans-serif",
       },
     },
