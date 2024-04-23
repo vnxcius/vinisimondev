2a7be5b7c897a86a13f64c1a24b3deda129f5ad6
vnxcius <vsgh-gouveia@hotmail.com>
2024-04-23 09:31:48 -0300

    add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <>
+    <App />
+    <SpeedInsights />
+  </>
 )
